8ab51f449650c68b30cab5745ee64a8462647723
Chris Scott <chris.scott@nesi.org.nz>
2019-07-29 15:47:17 +1200

    new plot script

---
+++ run/summary_plot.cpp
@@ -0,0 +1,246 @@
+// library for loading data for the summary plot
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <Eigen/Dense>
+
+#define APICAL_CUTOFF 0.8
+#define BASAL_CUTOFF 0.8
+
+// cell to cell connectivity
+// this_triangle, other_cell, other_triangle
+enum cell_conn{ \
+  tTri, oCell, oTri, \
+  CCONNCOUNT};
+
+
+struct Mesh {
+  int vertices_count;
+  Eigen::Array<double, Eigen::Dynamic, 3, Eigen::RowMajorBit> vertices; // 3x coordinate
+//  int surface_triangles_count;
+//  Eigen::Array<int, Eigen::Dynamic, 3, Eigen::RowMajorBit> surface_triangles; // 3x vertex
+  int tetrahedrons_count;
+  Eigen::Array<int, Eigen::Dynamic, 4, Eigen::RowMajorBit> tetrahedrons; // 4x vertex
+//  int common_triangles_count;
+//  Eigen::Array<int, Eigen::Dynamic, CCONNCOUNT, Eigen::RowMajorBit> common_triangles; // this triangle, other cell, other triangle
+//  int apical_triangles_count;
+//  Eigen::Array<int, Eigen::Dynamic, 1> apical_triangles; // surface triangle indicies
+//  int basal_triangles_count;
+//  Eigen::Array<int, Eigen::Dynamic, 1> basal_triangles; // surface triangle indicies
+  Eigen::Array<double, Eigen::Dynamic, 1> dfa; // distance from apical (per element)
+  Eigen::Array<double, Eigen::Dynamic, 1> dfb; // distance from basal (per element)
+  std::vector<int> apical_tets;
+  std::vector<int> basal_tets;
+  double apical_vol;
+  double basal_vol;
+  std::vector<double> apical_tets_vol;
+  std::vector<double> basal_tets_vol;
+};
+
+
+
+void fatal_error(std::string msg);
+void load_mesh(std::string file_name, struct Mesh& mesh);
+
+
+void fatal_error(std::string msg) {
+  std::cerr << msg << std::endl;
+  exit(1);
+}
+
+void load_mesh(std::string file_name, struct Mesh& mesh){
+  std::cout << "Loading mesh file: " << file_name << std::endl;
+
+  // local variables
+  std::ifstream cell_file(file_name.c_str(), std::ios::in | std::ios::binary); // open the mesh file
+  uint32_t i32;
+  float f32;
+
+  // check the file is open
+  if (not cell_file.is_open()) {
+    fatal_error("mesh file " + file_name + " could not be opened");
+  }
+  // get the mesh vertices (int32 count, 3x-float32 vertices) 
+  cell_file.read(reinterpret_cast<char *>(&i32), sizeof(i32));
+  mesh.vertices_count = i32;
+  std::cout << "  Number of vertices = " << mesh.vertices_count << std::endl;
+  mesh.vertices.resize(mesh.vertices_count, Eigen::NoChange);
+  for(int n=0; n<mesh.vertices_count; n++){
+    for(int m=0; m<3; m++){
+      cell_file.read(reinterpret_cast<char *>(&f32), sizeof(f32));
+      mesh.vertices(n,m) = f32;
+    }
+  }
+  // get the surface triangles (int32 count, 3x-int32 vertex indices)
+  cell_file.read(reinterpret_cast<char *>(&i32), sizeof(i32));
+  int surface_triangles_count = i32;
+//  mesh.surface_triangles.resize(mesh.surface_triangles_count, Eigen::NoChange);
+  for(int n=0; n<surface_triangles_count; n++){
+    for(int m=0; m<3; m++){
+      cell_file.read(reinterpret_cast<char *>(&i32), sizeof(i32));
+//      mesh.surface_triangles(n,m) = i32-1; // change to zero indexed
+    }
+  }
+  // get the element tetrahedrons (int32 count, 4x-int32 vertex indices, float32 dfa, float32 dfb)
+  cell_file.read(reinterpret_cast<char *>(&i32), sizeof(i32));
+  mesh.tetrahedrons_count = i32;
+  mesh.tetrahedrons.resize(mesh.tetrahedrons_count, Eigen::NoChange);
+  mesh.dfa.resize(mesh.tetrahedrons_count, Eigen::NoChange);
+  mesh.dfb.resize(mesh.tetrahedrons_count, Eigen::NoChange);
+  for(int n=0; n<mesh.tetrahedrons_count; n++){
+    for(int m=0; m<4; m++){
+      cell_file.read(reinterpret_cast<char *>(&i32), sizeof(i32));
+      mesh.tetrahedrons(n,m) = i32-1; // change to zero indexed
+    }
+    cell_file.read(reinterpret_cast<char *>(&f32), sizeof(f32));
+    mesh.dfa(n) = f32;
+    cell_file.read(reinterpret_cast<char *>(&f32), sizeof(f32));
+    mesh.dfb(n) = f32;
+  }
+  // get the apical triangles (int32 count, int32 triangle indices)
+  cell_file.read(reinterpret_cast<char *>(&i32), sizeof(i32));
+  int apical_triangles_count = i32;
+//  mesh.apical_triangles.resize(mesh.apical_triangles_count, Eigen::NoChange);
+  for(int n=0; n<apical_triangles_count; n++){
+    cell_file.read(reinterpret_cast<char *>(&i32), sizeof(i32));
+//    mesh.apical_triangles(n) = i32-1; // change to zero indexed
+  }
+  // get the basal triangles (int32 count, int32 triangle indices)
+  cell_file.read(reinterpret_cast<char *>(&i32), sizeof(i32));
+  int basal_triangles_count = i32;
+//  mesh.basal_triangles.resize(mesh.basal_triangles_count, Eigen::NoChange);
+  for(int n=0; n<basal_triangles_count; n++){
+    cell_file.read(reinterpret_cast<char *>(&i32), sizeof(i32));
+//    mesh.basal_triangles(n) = i32-1; // change to zero indexed
+  }
+  // get the cell-to-cell data (int32 count, 3x-int32 this_triamgle, other_cell, other_triangle)
+  cell_file.read(reinterpret_cast<char *>(&i32), sizeof(i32));
+  int common_triangles_count = i32;
+//  mesh.common_triangles.resize(mesh.common_triangles_count, Eigen::NoChange);
+  for(int n=0; n<common_triangles_count; n++){
+    for(int m=0; m<3; m++){
+      cell_file.read(reinterpret_cast<char *>(&i32), sizeof(i32));
+//     mesh.common_triangles(n,m) = i32-1; // change to zero indexed
+    }
+  }
+  cell_file.close();
+
+  // get the apical/basal tets
+  mesh.apical_tets.resize(mesh.tetrahedrons_count);
+  mesh.basal_tets.resize(mesh.tetrahedrons_count);
+  mesh.apical_tets_vol.resize(mesh.tetrahedrons_count);
+  mesh.basal_tets_vol.resize(mesh.tetrahedrons_count);
+  mesh.apical_vol = 0;
+  mesh.basal_vol = 0;
+  int nbasal = 0;
+  int napical = 0;
+  for (int n = 0; n < mesh.tetrahedrons_count; n++) {
+    // volume of tet
+    Eigen::Matrix<int,1,4> vi;      // tetrahedron vertex indices
+    vi = mesh.tetrahedrons.block<1,4>(n, 0);
+
+    Eigen::Matrix<double,4,3> vert; // tetrahedron vertex coordinates
+    for(int i = 0; i < 4; i++)
+      vert.block<1,3>(i, 0) = mesh.vertices.block<1,3>(int(vi(i)), 0); // why is typecast needed???
+
+    Eigen::Matrix<double,3,3> J;    // tetrahedron edge vectors
+    for(int i = 0; i < 3; i++)
+      J.block<1,3>(i, 0) = vert.block<1,3>(i + 1, 0) - vert.block<1,3>(0, 0);
+    double V, Vx6;                  // tetrahedron volume, (6x) volume
+    Vx6 = J.determinant();
+    V = Vx6 / 6.0;
+
+    // apical or basal?
+    if (mesh.dfa(n) < APICAL_CUTOFF) {
+      mesh.apical_tets_vol[napical] = V;
+      mesh.apical_tets[napical++] = n;
+      mesh.apical_vol += V;
+    }
+    if (mesh.dfb(n) < BASAL_CUTOFF) {
+      mesh.basal_tets_vol[napical] = V;
+      mesh.basal_tets[nbasal++] = n;
+      mesh.basal_vol += V;
+    }
+
+  }
+  mesh.apical_tets.resize(napical);
+  mesh.basal_tets.resize(nbasal);
+  mesh.apical_tets_vol.resize(napical);
+  mesh.basal_tets_vol.resize(nbasal);
+}
+
+float get_tet_value(struct Mesh& mesh, int index, float* node_values) {
+  Eigen::Array<int,1,4> vi;      // tetrahedron vertex indices
+  vi = mesh.tetrahedrons.block<1,4>(index, 0);
+  float vav  = 0.25 * (node_values[vi(0)]  + node_values[vi(1)]  + node_values[vi(2)]  + node_values[vi(3)]);
+
+  return vav;
+}
+
+void get_plot_data(std::string file_name, int ntime, struct Mesh& mesh, float* result_apical, float* result_basal) {
+  // each row is a time point, each column is a node
+  
+  std::ifstream result_file(file_name.c_str(), std::ios::in | std::ios::binary); // open the mesh file
+  if (result_file) {
+    int np = mesh.vertices_count;
+    int na = mesh.apical_tets.size();
+    int nb = mesh.basal_tets.size();
+    float *buffer = new float[np];
+    // loop over time steps
+    for (int t = 0; t < ntime; t++) {
+      // load value at each node for this time point
+      result_file.read(reinterpret_cast<char*>(buffer), np * sizeof(float));
+      
+//      for (int i = 0; i < np; i++) {
+//        float f32;
+//        result_file.read(reinterpret_cast<char*>(&f32), 1);
+
+
+      if (result_file) {
+        // get values at apical tets
+        result_apical[t] = 0.0;
+        for (int i = 0; i < na; i++) {
+          int index = mesh.apical_tets[i];
+          float val = get_tet_value(mesh, index, buffer);
+          result_apical[t] += mesh.apical_tets_vol[index] * val;
+        }
+        result_apical[t] /= mesh.apical_vol;
+
+        // get values at basal tets
+        result_basal[t] = 0.0;
+        for (int i = 0; i < nb; i++) {
+          int index = mesh.basal_tets[i];
+          float val = get_tet_value(mesh, index, buffer);
+          result_basal[t] += mesh.basal_tets_vol[index] * val;
+        }
+        result_basal[t] /= mesh.basal_vol;
+      }
+      else {
+        fatal_error("Error reading row");
+      }
+    }
+    result_file.close();
+  }
+  else {
+    fatal_error(std::string("Error loading result file: ") + file_name);
+  }
+}
+
+// function to be called from Python
+extern "C"
+int load_summary_plot_data(char* id, int ntime, float* ca_apical, float* ca_basal, float* ip_apical, float* ip_basal) {
+  std::string mesh_file = std::string(id) + std::string(".bmsh");
+  struct Mesh mesh;
+  load_mesh(mesh_file, mesh);
+
+  std::string ca_file = std::string(id) + std::string("_ca.bin");
+  get_plot_data(ca_file, ntime, mesh, ca_apical, ca_basal);
+
+  std::string ip_file = std::string(id) + std::string("_ip3.bin");
+  get_plot_data(ip_file, ntime, mesh, ip_apical, ip_basal);
+
+  return 0;
+}
---
+++ run/summary_plot2.py
@@ -0,0 +1,156 @@
+from __future__ import print_function
+
+import matplotlib as mpl
+mpl.use('Agg') # because there's no X11 display
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import struct
+import ctypes
+import time
+
+ncells = 7
+nintra = 8
+#dtypes = ["ca", "ip3", "cer", "volume"]
+dtypes = ["ca", "ip3", "volume"]
+plot_ffr = True
+
+
+##################################################################
+# ctypes lib for loading data
+##################################################################
+
+script_dir = os.path.abspath(os.path.dirname(__file__))
+_lib = ctypes.CDLL(os.path.join(script_dir, "_summary_plot.so"))
+_lib.load_summary_plot_data.restype = None
+_lib.load_summary_plot_data.argtypes = [
+  ctypes.c_char_p,
+  ctypes.c_int,
+  np.ctypeslib.ndpointer(dtype=np.float32),
+  np.ctypeslib.ndpointer(dtype=np.float32),
+  np.ctypeslib.ndpointer(dtype=np.float32),
+  np.ctypeslib.ndpointer(dtype=np.float32),
+]
+
+##################################################################
+# functions
+##################################################################
+
+def get_data_fluid_flow(ntime):
+  fname = "l1_results.dat"
+  data = np.loadtxt(fname)
+  assert data.shape[0] == ntime, "Wrong number of lines in fluid flow results"
+
+  vol_cols = [i * nintra + 0 for i in range(ncells)]
+  volumes = data[:, vol_cols]
+  ffr = data[:, -1]
+
+  return volumes, ffr
+
+def get_data(fname, rows, cols):
+  if _lib is None:
+    data = get_data_py(dname + '_' + dtype + ".bin", nodes, x.shape[0])
+  else:
+    data = get_data_c(dname + '_' + dtype + ".bin", nodes, x.shape[0])
+  return data
+
+## read in a simulation data file
+def get_data_c(fname, rows, cols):
+  data = np.zeros((2, cols), dtype=np.float32)
+  _lib.get_data(fname.encode("utf-8"), rows, cols, data)
+  return data
+
+## read in a simulation data file
+def get_data_py(fname, rows, cols):
+  f1 = open(fname, "rb")
+  data = np.zeros((2, cols), dtype=np.float32)
+  buf = np.zeros(rows, dtype=np.float32)
+  for c in range(cols):     # the data is in column order
+    for r in range(rows):
+      buf[r] = struct.unpack('f', f1.read(4))[0]
+    data[0, c] = buf.min()
+    data[1, c] = buf.max()
+  f1.close()
+  return data
+
+## get the time values associated with the saved data
+def get_time_vals(fname):
+  f = open(fname + ".dat", "r") # get the saved data stride
+  for line in f:
+    if "Tstride" in line:
+      tstride = int(line.rstrip().split()[1])
+      break
+  f.close()
+  tvals = [] # list of time values
+  n = int(0)
+  f = open(fname + ".out", "r")
+  for line in f:
+    if "<Acinus> t:" in line: # iterate through the saved time step values
+      if(float(line.rstrip().split()[2]) == 0.0): continue  # skip start time 0.0
+      n += 1
+      if (n % tstride) == 0:
+        tvals.append(float(line.rstrip().split()[2])) # add to list of time values
+  f.close()
+  return np.asarray(tvals) # return time values in a numpy array
+
+# get cell node count
+def get_node_count(fname):
+  f = open(fname, "r")
+  for line in f:
+    if line.startswith("<CellMesh> vertices_count:"):
+      v = line.rstrip().split()
+  f.close()
+  return int(v[2])
+
+
+##################################################################
+# main program
+##################################################################
+
+print("create summary plot")
+
+nplots = len(dtypes) + 1 if plot_ffr else len(dtypes)
+fig, plots = plt.subplots(nplots, ncells, sharex='col', squeeze=False)
+plt.subplots_adjust(wspace = 0.5)
+fig.set_size_inches(ncells * 3.8, nplots * 2.5)
+fig.text(0.02, 0.96, os.getcwd(), fontsize=10)
+
+x = get_time_vals("a1") # get the x-axis time values
+volumes, ffr = get_data_fluid_flow(x.shape[0])
+for cell in range(ncells):
+  dname = "a1c" + str(cell + 1)
+  nodes = get_node_count(dname + ".out")
+  if cell == 0 and plot_ffr:
+    pass
+  else:
+    plots[len(dtypes)-1, cell].set_xlabel(" time (s)")
+  plots[0, cell].set_title("Cell " + str(cell+1))
+  ca_apical = np.empty(x.shape[0], np.float32)
+  ca_basal = np.empty(x.shape[0], np.float32)
+  ip_apical = np.empty(x.shape[0], np.float32)
+  ip_basal = np.empty(x.shape[0], np.float32)
+  _lib.load_summary_plot_data(dname.encode("utf-8"), x.shape[0], ca_apical, ca_basal, ip_apical, ip_basal)
+  plots[0, cell].plot(x, ca_apical, color='blue', label='apical')
+  plots[0, cell].plot(x, ca_basal, color='red', label='basal')
+  plots[0, cell].legend(loc='best')
+  plots[1, cell].plot(x, ip_apical, color='blue', label='apical')
+  plots[1, cell].plot(x, ip_basal, color='red', label='basal')
+  plots[1, cell].legend(loc='best')
+  plots[2, cell].plot(x, volumes[:, cell], color='blue')
+
+for i, dtype in enumerate(dtypes):
+    if dtype == 'volume':
+        plots[i, 0].set_ylabel(dtype + " ($\mu$m$^3$)")
+    else:
+        plots[i, 0].set_ylabel(dtype + " ($\mu$M)")
+
+if plot_ffr:
+  for i in range(1, ncells):
+    plots[-1, i].axis('off')
+  plots[-1, 0].set_title("Total fluid flow rate")
+  plots[-1, 0].plot(x, ffr, color='blue')
+  plots[-1, 0].set_xlabel(" time (s)")
+  plots[-1, 0].set_ylabel(" Flow rate ($\mu m^3$/sec)")
+
+fig.savefig("summary_plot.pdf")
