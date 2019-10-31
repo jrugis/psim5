2e808abab0d5c2500ed974202b7c1d6adc85fa73
John Walter Rugis j.rugis@auckland.ac.nz <j.rugis@auckland.ac.nz>
2019-10-31 01:50:19 +0000

    rename mesh files

--- run/run_sim.py
+++ run/run_sim.py
@@ -115,7 +115,7 @@ if(len(sys.argv) >= 5):
     quit()
   p1_array, p2_array = get_sweep_parms(sweep)
 
-mesh_base = "4sim_out_N4_p3-p2-p4-Xtet.bin"
+mesh_base = "out_N4_p3-p2-p4-Xtet.bmsh"
 
 # create the top level results directory
 list = run_dir.split("/")[-4:]
